9a4400c32e62a875d869960dba7cf44f5b4c266b
Jongwoo Park <jongwoo.park@code.berlin>
2022-03-03 21:12:48 +0100

    added chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from '@chakra-ui/react'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <ChakraProvider>
       <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 };

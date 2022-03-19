32d8d5be92eadffd2e43e0868aa64545f56c9e6b
Jongwoo Park <jongwoo.park@code.berlin>
2022-03-19 17:32:30 +0100

    grid

--- components/AdditionalPost.tsx
+++ components/AdditionalPost.tsx
@@ -7,11 +7,17 @@ import {
   Stack,
   Image,
   Center,
+  AspectRatio,
 } from '@chakra-ui/react'
 import Router from 'next/router'
 
 const AdditionalPost = ({ myPost }) => {
-  console.log(myPost)
+  // console.log(myPost)
+  // random integer generator
+  const rndNum = (max) => {
+    return Math.floor(Math.random() * Math.floor(max))
+  }
+
   return (
     <Box
       p="2"
@@ -25,15 +31,23 @@ const AdditionalPost = ({ myPost }) => {
         .map((post) => (
           <Box
             boxShadow="md"
-            my="2"
+            mb="4"
+            w="100%"
             display="inline-block"
             onClick={() => Router.push('/p/[id]', `/p/${post.id}`)}
           >
             <Box key={post.id} border="2px solid gray" borderRadius="md" p="2">
-              <Text>{post.title}</Text>
-              <Center borderRadius="md" overflow="clip">
-                <Image w="50%" src={post.imageUrl} overflow="clip" />
-              </Center>
+              <Text fontSize="xl">{post.title}</Text>
+              {post.imageUrl && (
+                <AspectRatio ratio={1} borderRadius="md" overflow="clip">
+                  <Image
+                    boxSize="50%"
+                    src={post.imageUrl}
+                    borderRadius="md"
+                    overflow="clip"
+                  />
+                </AspectRatio>
+              )}
               <Text noOfLines={1}>{post.content}</Text>
             </Box>
           </Box>

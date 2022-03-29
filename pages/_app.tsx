5b85b857adafc17101f74aeaef708ccf28be8320
Jongwoo Park <jongwoo.park@code.berlin>
2022-03-29 11:42:47 +0200

    scroll Y preservation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { ChakraProvider } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { useScrollRestoration } from '../utils/useScrollRestoration'
 import { MediaContextProvider } from '../utils/media'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  // scroll posY preseervation
+  const router = useRouter()
+  useScrollRestoration(router)
   return (
     <SessionProvider session={pageProps.session}>
       <MediaContextProvider>

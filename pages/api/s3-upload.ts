849bcaaa342977f60d0d0ecd219953a681d84588
Jongwoo Park <jongwoo.park@code.berlin>
2022-04-15 21:58:57 +0200

    -

--- pages/api/s3-upload.ts
+++ pages/api/s3-upload.ts
@@ -1,9 +1,14 @@
 export { APIRoute as default } from 'next-s3-upload'
-// // pages/api/s3-upload.js
+
 // import { APIRoute } from 'next-s3-upload'
+
+// const date = () => {
+//   const d = new Date()
+//   return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`
+// }
 
 // export default APIRoute.configure({
 //   key(req, filename) {
-//     return `bandtent-db/uploads/path/${filename.toUpperCase()}`
+//     return `${date}/${filename.toUpperCase()}`
 //   },
 // })

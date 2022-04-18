4a20eead02924620d38d61f176030a415f5620e1
Jongwoo Park <jongwoo.park@code.berlin>
2022-04-18 21:34:08 +0200

    msw worker fixed

--- mocks/index.ts
+++ mocks/index.ts
@@ -3,9 +3,14 @@ const IS_BROWSER = typeof window !== 'undefined'
 export const setupMocks = async () => {
   if (IS_BROWSER) {
     const { mswWorker } = await import('./mswWorker')
-    mswWorker.start()
+    mswWorker.start({
+      waitUntilReady: true,
+      // onUnhandledRequest: 'bypass',
+    })
+    mswWorker.printHandlers()
   } else {
     const { mswServer } = await import('./mswServer')
     mswServer.listen()
+    mswServer.printHandlers()
   }
 }

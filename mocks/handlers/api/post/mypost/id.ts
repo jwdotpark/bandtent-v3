1a8a2803de2946815f2760c787d5eb4e0ac197d5
Jongwoo Park <jongwoo.park@code.berlin>
2022-04-18 14:47:14 +0200

    wip: mypost handler added

--- mocks/handlers/api/post/mypost/id.ts
+++ mocks/handlers/api/post/mypost/id.ts
@@ -15,12 +15,12 @@ const myPost = {
     {
       id: 134,
       title: 'Test artist',
-      content: 'test title',
+      content: 'some audio',
       published: true,
       authorId: 1,
       createdAt: '2022-04-17T13:49:17.058Z',
       imageUrl: 'https://placekitten.com/400/400',
-      fileUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+      fileUrl: 'https://freesound.org/data/previews/628/628608_13598778-lq.mp3',
     },
     {
       id: 133,
@@ -30,13 +30,13 @@ const myPost = {
       authorId: 1,
       createdAt: '2022-04-17T13:18:24.773Z',
       imageUrl: 'https://placekitten.com/400/400',
-      fileUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+      fileUrl: 'https://freesound.org/data/previews/628/628608_13598778-lq.mp3',
     },
   ],
 }
 
 export const myPostHandler = [
-  // every request of myPost will return the same response
+  // same response with every request
   rest.get('/api/post/mypost/:id', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(myPost))
   }),

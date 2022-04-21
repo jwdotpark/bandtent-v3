6ca4080260db31202207430a43d6d252c8c16144
Jongwoo Park <jongwoo.park@code.berlin>
2022-04-21 17:20:00 +0200

    -

--- mocks/index.ts
+++ mocks/index.ts
@@ -3,14 +3,13 @@ const IS_BROWSER = typeof window !== 'undefined'
 export const setupMocks = async () => {
   if (IS_BROWSER) {
     const { mswWorker } = await import('./mswWorker')
+    console.log('browser worker start')
     mswWorker.start({
       waitUntilReady: true,
-      // onUnhandledRequest: 'bypass',
     })
-    // mswWorker.printHandlers()
   } else {
     const { mswServer } = await import('./mswServer')
+    console.log('server worker start')
     mswServer.listen()
-    // mswServer.printHandlers()
   }
 }

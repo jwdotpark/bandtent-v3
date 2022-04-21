809c8938fcf4019a2a90eb7f6970c2b09669aad8
Jongwoo Park <jongwoo.park@code.berlin>
2022-04-21 17:41:31 +0200

    draft page testing

--- __tests__/draft.test.jsx
+++ __tests__/draft.test.jsx
@@ -1,19 +1,29 @@
 import { render, screen, waitFor } from './test-utils'
 import Draft from '../pages/create'
-// import { waitFor } from '@testing-library/react'
 
 describe('Create page', () => {
-  it('is rendered', async () => {
+  it('form is rendered', async () => {
     render(<Draft />)
-    
+
     const newItem = await screen.findByText('New Item')
     const artist = await screen.findByText('Artist')
     const title = await screen.findByText('Title')
+    const image = await screen.findByText('Add Audio(*.png, *.jpg, *.bmp)')
+    const file = await screen.findByText(
+      'Add Music(*.mp3, *.wav, *.aiff, *.ogg)'
+    )
+    const submitButton = await screen.findByText('Not available')
+    const cancelButton = await screen.findByText('Cancel')
 
     await waitFor(() => {
       expect(newItem).toBeInTheDocument()
       expect(artist).toBeInTheDocument()
       expect(title).toBeInTheDocument()
+      expect(image).toBeInTheDocument()
+      expect(file).toBeInTheDocument()
+      expect(submitButton).toBeInTheDocument()
+      expect(submitButton).toHaveAttribute('disabled')
+      expect(cancelButton).toBeInTheDocument()
     })
   })
 })

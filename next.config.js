518ef4e6e2ca9113e834487446a504b68ef23ea0
Jongwoo Park <jongwoo.park@code.berlin>
2022-04-23 16:51:39 +0200

    prisma client updatd

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const nextConfig = {
   /* config options here */
   // compress: true,
   // swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('_http_common')
+    }
+    return config
+  },
+  target: 'serverless',
 }
 
 module.exports = nextConfig

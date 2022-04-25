ec31abefc43ce27669ff978d77e0f95319eb5cad
Jongwoo Park <jongwoo.park@code.berlin>
2022-04-25 13:00:51 +0200

    serverless config changed

--- next.config.js
+++ next.config.js
@@ -7,13 +7,36 @@ const nextConfig = {
   /* config options here */
   compress: true,
   swcMinify: true,
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     config.externals.push('_http_common')
-  //   }
-  //   return config
-  // },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('_http_common')
+    }
+    return config
+  },
   // target: 'serverless',
+  target: 'experimental-serverless-trace',
+  env: {
+    S3_UPLOAD_KEY: process.env.S3_UPLOAD_KEY,
+    S3_UPLOAD_SECRET: process.env.S3_UPLOAD_SECRET,
+    S3_UPLOAD_BUCKET: process.env.S3_UPLOAD_BUCKET,
+    S3_UPLOAD_REGION: process.env.S3_UPLOAD_REGION,
+    DATABASE_URL_WITH_SCHEMA: process.env.DATABASE_URL_WITH_SCHEMA,
+    DATABASE_URL: process.env.DATABASE_URL,
+    SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
+    EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
+    EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
+    EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
+    EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    SECRET: process.env.SECRET,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 }
 
 module.exports = nextConfig

baeacf0ad26367cdccb029e05033025627241005
Jongwoo Park <jongwoo.park@code.berlin>
2022-04-25 13:59:14 +0200

    changed prisma version & schema binary target added

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
 // eslint-disable-next-line
 
+// https://github.com/serverless-nextjs/serverless-next.js/issues/1749
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   /* config options here */
-  compress: true,
-  swcMinify: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('_http_common')
-    }
-    return config
-  },
-  // target: 'serverless',
+  // reactStrictMode: true,
+  // compress: true,
+  // swcMinify: true,
   target: 'experimental-serverless-trace',
   env: {
     S3_UPLOAD_KEY: process.env.S3_UPLOAD_KEY,
@@ -36,7 +33,36 @@ const nextConfig = {
     SECRET: process.env.SECRET,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    PRISMA_QUERY_ENGINE_BINARY: process.env.PRISMA_QUERY_ENGINE_BINARY,
   },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('_http_common')
+    }
+    return config
+  },
+
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.externals.push('_http_common')
+  //   }
+  //   config.plugins.push(
+  //     new CopyWebpackPlugin({
+  //       patterns: [
+  //         {
+  //           from: '../../node_modules/.prisma/client/schema.prisma',
+  //           to: './',
+  //         },
+  //         {
+  //           from: '../../node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node',
+  //           to: './',
+  //         },
+  //       ],
+  //     })
+  //   )
+  //   return config
+  // },
 }
 
 module.exports = nextConfig

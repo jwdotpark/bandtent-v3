6569b8a53e3a7d45c01af77fdbb8c61c03f94132
Jongwoo Park <jongwoo.park@code.berlin>
2022-05-14 17:17:34 +0200

    Sat May 14 17:17:28 CEST 2022

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -8,7 +8,7 @@ context('search input in the navbar', () => {
       .type('test')
       .click(cy.get('.chakra-input__right-element > .chakra-button'))
 
-    cy.get('.css-jb7eng').should('be.visible').click()
+    cy.get('.css-jb7eng').should('be.visible').click({ multiple: true })
     cy.get('.css-1cq3f0e').should('be.visible')
 
     cy.get(
@@ -23,6 +23,4 @@ context('search input in the navbar', () => {
       .click(cy.get('.chakra-input__right-element > .chakra-button'))
     cy.get(':nth-child(2) > .chakra-text > b').should('not.be.visible')
   })
-
-  
 })

44488b59ae4e1060cea5d0d10b5165025bf19193
Jongwoo Park <jongwoo.park@code.berlin>
2022-05-14 16:20:12 +0200

    Sat May 14 16:15:41 CEST 2022

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,11 +1,14 @@
 const scanner = require('sonarqube-scanner')
+require('dotenv').config()
+
+console.log(process.env)
 
 scanner(
   {
     serverUrl: 'http://localhost:9000',
-    // login: process.env.SONARQUBE_ID,
-    // password: process.env.SONARQUBE_PASSWORD,
-    token: 'a04c6b7cfa5d9d4d3ffa26a788c08dd2dd89d179',
+    // login: 'admin',
+    // password: 'admin',
+    token: process.env.SONARQUBE_TOKEN,
     options: {
       'sonar.sources': './',
     },

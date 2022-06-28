7e31095f8131faa24496f58711fff0443dd834c6
Jongwoo Park <jongwoo.park@code.berlin>
2022-06-28 23:08:41 +0200

    Tue Jun 28 23:08:16 CEST 2022

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -37,10 +37,6 @@ const options = {
   secret: process.env.SECRET,
   pages: {
     signIn: '/auth/signin',
-    // signOut: '/auth/signout',
-    // error: '/auth/error', // Error code passed in query string as ?error=
-    // verifyRequest: '/auth/verify-request', // (used for check email message)
-    // newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   callbacks: {
     session({ session, token, user }) {

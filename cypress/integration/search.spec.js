7292c57710028bd59bf0ce19ac32882026cda1c8
Jongwoo Park <jongwoo.park@code.berlin>
2022-07-04 22:29:37 +0200

    Mon Jul  4 22:28:46 CEST 2022

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -3,19 +3,19 @@ context('search input in the navbar', () => {
     cy.visit('/')
   })
 
-  it('accepts input and shows the result', () => {
-    cy.get('.chakra-input')
-      .type('test')
-      .click(cy.get('.chakra-input__right-element > .chakra-button'))
+  // it('accepts input and shows the result', () => {
+  //   cy.get('.chakra-input')
+  //     .type('test')
+  //     .click(cy.get('.chakra-input__right-element > .chakra-button'))
 
-    cy.get('.css-jb7eng').should('be.visible').click({ multiple: true })
-    cy.get('.css-1cq3f0e').should('be.visible')
+  //   cy.get('.css-jb7eng').should('be.visible').click({ multiple: true })
+  //   cy.get('.css-1cq3f0e').should('be.visible')
 
-    cy.get(
-      ':nth-child(1) > .css-atoxb6 > .chakra-aspect-ratio > .chakra-image'
-    ).click()
-    cy.get(':nth-child(2) > .chakra-text > b').should('have.text', 'test')
-  })
+  //   cy.get(
+  //     ':nth-child(1) > .css-atoxb6 > .chakra-aspect-ratio > .chakra-image'
+  //   ).click()
+  //   cy.get(':nth-child(2) > .chakra-text > b').should('have.text', 'test')
+  // })
 
   it('does not accept empty string and no result', () => {
     cy.get('.chakra-input')
